635119f5c999638610f33371f0928f08459f3a54
Artem Ryzhkov <digital_boy@mail.ru>
2019-05-17 04:50:47 +0700

    $ composer require annotation Using version ^5.3 for sensio/framework-extra-bundle ./composer.json has been updated Loading composer repositories with package information Updating dependencies (including require-dev) Restricting packages listed in "symfony/symfony" to "4.2.*" Package operations: 1 install, 0 updates, 0 removals   - Installing sensio/framework-extra-bundle (v5.3.1): Loading from cache Writing lock file Generating autoload files ocramius/package-versions:  Generating version class... ocramius/package-versions: ...done generating version class Symfony operations: 1 recipe (be4c8216c9e9a1f1a44674370013c560)   - Configuring sensio/framework-extra-bundle (>=5.2): From github.com/symfony/recipes:master

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
