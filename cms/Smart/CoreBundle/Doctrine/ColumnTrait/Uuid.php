afba27686c8cfbc990af02347664aea370c50e9d
Artem Ryzhkov <digital_boy@mail.ru>
2019-11-20 20:01:57 +0700

    add Ramsey Uuid

---
+++ cms/Smart/CoreBundle/Doctrine/ColumnTrait/Uuid.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Smart\CoreBundle\Doctrine\ColumnTrait;
+
+use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\UuidInterface;
+
+/**
+ * Uuid column
+ */
+trait Id
+{
+    /**
+     * @var UuidInterface
+     *
+     * @ORM\Id
+     * @ORM\Column(type="uuid", unique=true)
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
+     */
+    protected $id;
+
+    /**
+     * @return UuidInterface
+     */
+    public function getId(): UuidInterface
+    {
+        return $this->id;
+    }
+}
