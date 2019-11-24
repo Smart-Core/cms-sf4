69bd03ff37472b43ac95bd415bb4e98e2a553729
Artem Ryzhkov <digital_boy@mail.ru>
2019-11-24 18:51:35 +0700

    add UserNotNull column

---
+++ cms/Smart/CoreBundle/Doctrine/ColumnTrait/UserNotNull.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Smart\CoreBundle\Doctrine\ColumnTrait;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+trait UserNotNull
+{
+    /**
+     * @var UserInterface
+     *
+     * @ORM\ManyToOne(targetEntity="Symfony\Component\Security\Core\User\UserInterface")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    protected $user;
+
+    /**
+     * @return UserInterface
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param UserInterface $user
+     *
+     * @return $this
+     */
+    public function setUser(UserInterface $user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
